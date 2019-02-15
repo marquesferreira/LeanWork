898df95ce120ad824635c7d578f9dd8a5c1d25f2
marquesferreira <edivvas@hotmail.com>
2019-02-15 15:32:26 -0200

    salvando em local storage

--- js/mainController/mainController.js
+++ js/mainController/mainController.js
@@ -1,18 +1,18 @@
 
 app.controller("controllerForm", function($scope){
-$scope.nome;
-$scope.email;
-$scope.telefone;
-$scope.cpf;
+// $scope.nome;
+// $scope.email;
+// $scope.telefone;
+// $scope.cpf;
 
-
-
+//objeto pessoa
+$scope.pessoa = {};
+//array de objetos pessoa
+$scope.pessoas = [];
 //desativa botão
 $scope.botaoAtivo = false;
-$scope.pessoas = [
 
-]
-$scope.incluirPessoa = function(){
+$scope.incluirPessoa = function(pessoa){
     //se formulário é inválido encerra a funcão antes de salvar
     if($scope.meuFormulario.$invalid){
         return;
@@ -20,21 +20,28 @@ $scope.incluirPessoa = function(){
     //adiciona Pessoa a Lista de Pessoas
 
     
-$scope.pessoas.push = ({nome:$scope.form.nome, 
-     email:$scope.form.email, 
-     cpf:$scope.form.cpf,tel: $scope.form.telefone });
-    console.log($scope.pessoas);
+$scope.pessoas.push = (angular.copy(pessoa));
+console.log(angular.toJson($scope.pessoas));
+var teste = [{}];
+var teste = angular.toJson(pessoa);
 
-
-    
+console.log("stringify:");
+console.debug(teste);
+delete $pessoa;
 //zera valores do form
-$scope.form = [{
+$scope.pessoa = [{
     nome: '',
     email: '',
     cpf:'',
     telefone:''
 
-}]
+}];
+
+salvaLocal();
+}
+//fim da funcao incluir pessoa
+function salvaLocal(){
+// localStorage.setItem('pessoasVsJSON', JSON.stringify(($scope.pessoas)));
 
 }
 
